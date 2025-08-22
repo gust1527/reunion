aa3873961f4bf89907a0514d46988e50102cef93
gust1527 <gustavemilholmsimonsen@gmail.com>
2025-08-22 22:48:56 +0200

    Updated with direct Google Drive links

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -25,7 +25,7 @@ export const photos: Photo[] = [
 		id: "01-studiearbejde-i-klassen",
 		title: "Studiearbejde i fælleren",
 		thumbnail: "/images/01-studiearbejde-i-klassen.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER1",
+		uploadLink: "https://drive.google.com/drive/folders/1HSQFCJY7iIuiUX9J815_sFTrnjyHJsYn?usp=sharing",
 		description: "Gustav og Jeppe arbejder(?) på deres computere.",
 		year: 2018
 	},
@@ -33,7 +33,7 @@ export const photos: Photo[] = [
 		id: "02-farverig-outfit",
 		title: "Farverigt outfit",
 		thumbnail: "/images/02-farverig-outfit.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER2",
+		uploadLink: "https://drive.google.com/drive/folders/1Q9aJOlRdViWfvLY7STY1x2j8ez_Bcbae?usp=sharing",
 		description: "Ørnen står i farverigt tøj og sneakers og poser på herretoilettet.",
 		year: 2018
 	},
@@ -41,7 +41,7 @@ export const photos: Photo[] = [
 		id: "03-venner-til-fest",
 		title: "Venner til fest",
 		thumbnail: "/images/03-venner-til-fest.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER3",
+		uploadLink: "https://drive.google.com/drive/folders/1QGOsfH9rWCen1LOp-jzDLZD1XsX8CpFX?usp=sharing",
 		description: "To personer står tæt og smiler til kameraet til en fest.",
 		year: 2018
 	},
@@ -49,7 +49,7 @@ export const photos: Photo[] = [
 		id: "04-gruppearbejde",
 		title: "Gruppearbejde",
 		thumbnail: "/images/04-gruppearbejde.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER4",
+		uploadLink: "https://drive.google.com/drive/folders/1MrW4dPave38dhG_kOFCMf_XH-woLzB9K?usp=sharing",
 		description: "Flere personer sidder samlet om borde med papir og computere.",
 		year: 2018
 	},
@@ -57,7 +57,7 @@ export const photos: Photo[] = [
 		id: "05-t-shirt-posering",
 		title: "T-shirt posering",
 		thumbnail: "/images/05-t-shirt-posering.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER5",
+		uploadLink: "https://drive.google.com/drive/folders/1GAwTZ1zQkPqrWbu3Hm_ixYpDWOUc1UvB?usp=sharing",
 		description: "En person står og poserer i en t-shirt.",
 		year: 2018
 	},
@@ -65,7 +65,7 @@ export const photos: Photo[] = [
 		id: "06-afslapning-paa-baenk",
 		title: "Afslapning på bænk",
 		thumbnail: "/images/06-afslapning-paa-baenk.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER6",
+		uploadLink: "https://drive.google.com/drive/folders/1hieG7Ipnv5G02ihGfmuFBkWxXsSEsFzK?usp=sharing",
 		description: "En person ligger afslappet hen over en træbænk.",
 		year: 2018
 	},
@@ -73,7 +73,7 @@ export const photos: Photo[] = [
 		id: "07-balance-paa-sten",
 		title: "Balance på sten",
 		thumbnail: "/images/07-balance-paa-sten.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER7",
+		uploadLink: "https://drive.google.com/drive/folders/1scp9aS8x6NZrMk_bYbt_gEpZZ_gAw0p3?usp=sharing",
 		description: "En person sidder på store sten udenfor og laver håndtegn.",
 		year: 2018
 	},
@@ -81,7 +81,7 @@ export const photos: Photo[] = [
 		id: "08-udklaedte-i-klassen",
 		title: "Udklædte i klassen",
 		thumbnail: "/images/08-udklaedte-i-klassen.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER8",
+		uploadLink: "https://drive.google.com/drive/folders/1zoWLWXPEGXkHp2SQkMYcta9XzeeKww5t?usp=sharing",
 		description: "Flere personer i udklædning sidder sammen i et klasselokale.",
 		year: 2018
 	},
@@ -89,7 +89,7 @@ export const photos: Photo[] = [
 		id: "09-laboratorie-notater",
 		title: "Laboratorie – notater",
 		thumbnail: "/images/09-laboratorie-notater.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER9",
+		uploadLink: "https://drive.google.com/drive/folders/1IKX16Co2gBdM_XJ8T3xgLMsNO_DjgyGq?usp=sharing",
 		description: "En person i kittel holder papir i et laboratorium.",
 		year: 2019
 	},
@@ -97,7 +97,7 @@ export const photos: Photo[] = [
 		id: "10-laboratorie-pipette",
 		title: "Laboratorie – pipette",
 		thumbnail: "/images/10-laboratorie-pipette.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER10",
+		uploadLink: "https://drive.google.com/drive/folders/1Ty1RWwXvCPsdzgoH8P0tcmczZcTItI4j?usp=sharing",
 		description: "En person i kittel arbejder med pipette i laboratoriet.",
 		year: 2019
 	},
@@ -105,7 +105,7 @@ export const photos: Photo[] = [
 		id: "11-laboratorie-forsoegsudstyr",
 		title: "Laboratorie – forsøgsudstyr",
 		thumbnail: "/images/11-laboratorie-forsoegsudstyr.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER11",
+		uploadLink: "https://drive.google.com/drive/folders/1xoece7j4xQQfukhCVElcR1HrmsfjFq1j?usp=sharing",
 		description: "En person viser et glas dækket med sølvfolie i laboratoriet.",
 		year: 2019
 	},
@@ -113,7 +113,7 @@ export const photos: Photo[] = [
 		id: "12-gruppe-ved-computer",
 		title: "Gruppe ved computer",
 		thumbnail: "/images/12-gruppe-ved-computer.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER12",
+		uploadLink: "https://drive.google.com/drive/folders/1a5H6JpdgisPGe_AUKPv__855aEr4RqzO?usp=sharing",
 		description: "Flere personer sidder samlet omkring en computer.",
 		year: 2019
 	},
@@ -121,7 +121,7 @@ export const photos: Photo[] = [
 		id: "13-selfie-i-spejl",
 		title: "Selfie i spejl",
 		thumbnail: "/images/13-selfie-i-spejl.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER13",
+		uploadLink: "https://drive.google.com/drive/folders/13sNvit5OsWaCDKQ_1MQaMxmbSNBWc4sx?usp=sharing",
 		description: "En person tager et selfie i et spejl.",
 		year: 2019
 	},
@@ -129,34 +129,10 @@ export const photos: Photo[] = [
 		id: "14-laboratorie-samarbejde",
 		title: "Laboratorie – samarbejde",
 		thumbnail: "/images/14-laboratorie-samarbejde.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER14",
+		uploadLink: "https://drive.google.com/drive/folders/1MMJxWv3YhyrPST1WAym8F4B8A2K-OcJA?usp=sharing",
 		description: "To personer i kittel arbejder sammen i laboratoriet.",
 		year: 2019
 	},
-	{
-		id: "15-haender-i-laboratoriet",
-		title: "Hænder i laboratoriet",
-		thumbnail: "/images/15-haender-i-laboratoriet.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER15",
-		description: "Hænder arbejder med laboratorieudstyr.",
-		year: 2019
-	},
-	{
-		id: "16-laesning-og-studier",
-		title: "Læsning og studier",
-		thumbnail: "/images/16-laesning-og-studier.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER16",
-		description: "En person læser og studerer ved et bord.",
-		year: 2019
-	},
-	{
-		id: "17-hoodie-og-afslappet-stil",
-		title: "Hoodie og afslappet stil",
-		thumbnail: "/images/17-hoodie-og-afslappet-stil.jpg",
-		uploadLink: "https://photos.app.goo.gl/PLACEHOLDER17",
-		description: "En person i hoodie med afslappet stil.",
-		year: 2019
-	}
 ];
 
 /**
